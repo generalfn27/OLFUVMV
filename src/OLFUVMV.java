444048c305b420c615630c04659549fc72342f42
Felix <generalfelix27@gmail.com>
2024-09-04 14:47:23 +0800

    OOPR1 Week 3: Assignment No. 1

--- src/OLFUVMV.java
+++ src/OLFUVMV.java
@@ -1,5 +1,28 @@
-public class Main {
+public class OLFUVMV {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Vision");
+        System.out.println("A premier inclusive university of choice aspiring to improve man as man by developing individuals ");
+        System.out.println("through a legacy of excellent education and compassionate value formation.\n");
+
+        System.out.println("Mission");
+        System.out.println("The Our Lady of Fatima University, together with Fatima Medical Science Foundation, Inc., is dedicated ");
+        System.out.print("to the improvement of man as man through the holistic formation of individuals imbued with knowledge, skills, and virtues.\n");
+
+        System.out.println("\nOLFU Core Values");
+        System.out.println("R – Respect");
+        System.out.println("I –  Integrity");
+        System.out.println("S – Service");
+        System.out.println("E – Excellence\n");
+
+        System.out.println("OLFU Graduate Attributes");
+        System.out.println("A – Aspirational");
+        System.out.println("C – Compassionate");
+        System.out.println("H – Honorable");
+        System.out.println("I – Inspiring");
+        System.out.println("E – Ethical");
+        System.out.println("V – Visionary");
+        System.out.println("E – Efficient");
+        System.out.println("R – Responsible");
     }
 }
